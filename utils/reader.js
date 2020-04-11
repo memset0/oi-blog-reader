f220657ca5152503d10d4880431767ea03673c11
memset0 <memset0@outlook.com>
2020-04-11 17:38:12 +0800

    style: 统一使用2空格缩进

--- utils/reader.js
+++ utils/reader.js
@@ -6,20 +6,20 @@ const FileSync = require("lowdb/adapters/FileSync");
 let db = lowdb(new FileSync(path.join(__dirname, "../data/posts.json")));
 
 function listPosts() {
-	let result = [];
-	db.sortBy(o => o.time)
-		.each(val => result.push(val))
-		.value();
-	return result;
+  let result = [];
+  db.sortBy(o => o.time)
+    .each(val => result.push(val))
+    .value();
+  return result;
 }
 
 function queryPost(id) {
-	return Object.assign(db.get(id).value(), {
-		content: fs.readFileSync(path.join(__dirname, `../public/source/${id}.html`), 'utf8').toString()
-	});
+  return Object.assign(db.get(id).value(), {
+    content: fs.readFileSync(path.join(__dirname, `../public/source/${id}.html`), 'utf8').toString()
+  });
 }
 
 module.exports = {
-	listPosts: listPosts,
-	queryPost: queryPost,
+  listPosts: listPosts,
+  queryPost: queryPost,
 };
